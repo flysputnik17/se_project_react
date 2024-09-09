af4f45e40e725cb173e9f636449b0b9a98503c8e
KODKOD177 <pavel2012155@gmail.com>
2024-09-09 16:49:19 +0300

    baseUrl update url

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,6 @@
 export const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://se-project-express-alpha.vercel.app/"
+    ? "https://se-project-express-alpha.vercel.app"
     : "http://localhost:3001";
 
 export default class Auth {
